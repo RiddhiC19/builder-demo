fd920fefba489c9c8d28c6343a0dd20cda8c71be
RiddhiC19 <riddhi.chhasatia@cygbit.com>
2022-11-01 17:34:09 +0530

    minor change

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -11,7 +11,7 @@ function HeroSection(){
             Maecenas sed diam eget risus varius blandit sit amet non magna.
             </div>
             <div className="Hero-description text-base text-slate-600 font-normal">
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Integer posuere erat a ante venenatis dapibus posuere velit
             </div>
             <div className="guide-btn my-12">
              <span className='rounded-3xl bg-black  text-white text-sm font-semibold uppercase px-5 py-4'>GEt the free guide!</span>
